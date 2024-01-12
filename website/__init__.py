a7762392bc4eb85b3204ecb706a3722f85f0ffea
Angelo Belfort <belfortangelo93@gmail.com>
2024-01-12 14:08:31 -0500

    create and config database

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# create database 
+db = SQLAlchemy()
+DB_NAME = 'items.db'
 
 def create_app():
     # Create the flask app
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # import and register blueprint
     from .views import views
+    from .auth import auth
     app.register_blueprint(views, url_prefix='/')
-
+    app.register_blueprint(auth, url_prefix='/')
+    
+    from .models import ItemsModel
+    
+    # create database for app
+    with app.app_context():
+        db.create_all()
+    
     return app
 
