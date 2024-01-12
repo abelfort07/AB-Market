dd2b9a3cd5f889e844ef6c9b5d1d0d594f34b9aa
Angelo Belfort <belfortangelo93@gmail.com>
2024-01-12 14:09:59 -0500

    Add market route to views.

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from .models import ItemsModel
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -7,4 +9,11 @@ views = Blueprint('views', __name__)
 @views.route('/home')
 def home_page():
     return render_template('home.html')
+
+
+@views.route('/market')
+def market_page():
+    # items_query = ItemsModel.query.all()
+    # print(items_query)
+    return render_template('market.html')
 
