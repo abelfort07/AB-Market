b5fbaf02b7cca380f9f6b0d8bf3b3277e9ff455a
Angelo Belfort <belfortangelo93@gmail.com>
2024-01-13 14:28:25 -0500

    create user table model and relationship with the items table model

--- website/models.py
+++ website/models.py
@@ -1,10 +1,27 @@
 from . import db
 
 
-class ItemsModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    barcode = db.Column(db.String(15), unique=True)
-    price = db.Column(db.Integer)
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+    user_budget = db.Column(db.Integer(), nullable=False, default=10000)
+    items = db.relationship('Items', backref='owned_user', lazy=True)
+    
+    def __repr__(self) -> str:
+        return f'User  {self.username}'
+
+
+class Items(db.Model):
+    id = db.Column(db.Integer( ), primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    barcode = db.Column(db.String(15), unique=True, nullable=False)
+    price = db.Column(db.Integer(), nullable=False)
+    description = db.Column(db.String(1024), unique=True, nullable=False)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    
+    def __repr__(self) -> str:
+        return f'Item  {self.name}'
 
 
