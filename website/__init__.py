80e469c5dcc68eeee3fed9513e830ef85a1cf44b
Angelo Belfort <belfortangelo93@gmail.com>
2024-01-13 14:27:36 -0500

    change app context structure in init file

--- website/__init__.py
+++ website/__init__.py
@@ -10,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    app.app_context().push()
 
     # import and register blueprint
     from .views import views
@@ -17,11 +18,8 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
-    from .models import ItemsModel
-    
-    # create database for app
-    with app.app_context():
-        db.create_all()
+    from .models import Items
+        
     
     return app
 
