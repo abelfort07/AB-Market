063c985af7e2fe57cb6aed1fccdd57c325f35c8d
Angelo Belfort <belfortangelo93@gmail.com>
2024-01-16 08:40:21 -0500

    Create register page and forms

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,10 @@
 from flask import Blueprint
+from flask import render_template
+from website.forms import RegisterForm
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/register')
+def register_page():
+    register_form = RegisterForm()
+    return render_template('register.html', form=register_form )
