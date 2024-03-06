751c7c06fc6bd06bdc89814fc66f1f6a8225ee79
Angelo Belfort <belfortangelo93@gmail.com>
2024-03-06 18:22:39 -0500

    validate username to avoid unique constraint error in database

--- website/forms.py
+++ website/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
+from website.models import User
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Username : ')
-    email = StringField(label='Email Address : ')
-    password1 = PasswordField(label='Password : ')
-    password2 = PasswordField(label='Confirm Password : ')
+    
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check).first()
+    
+        if user:
+            ...
+            
+    username = StringField(label='Username : ', validators=[Length(min=2, max=30), DataRequired()])
+    email = StringField(label='Email Address : ', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password : ', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password : ', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')   
