3f93d7ffbed8b5cff5843a363c0584951a55a7ee
Angelo Belfort <belfortangelo93@gmail.com>
2024-03-09 12:42:39 -0500

    update usename field in register. create route for login form

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, flash
 from flask import render_template, redirect, url_for
-from website.forms import RegisterForm
+from website.forms import RegisterForm, LoginForm
 from website.models import User
 from website import db
 
@@ -10,7 +10,7 @@ auth = Blueprint('auth', __name__)
 def register_page():
     register_form = RegisterForm()
     if register_form.validate_on_submit():
-        user_to_create = User(username=register_form.username.data, email=register_form.email.data, password=register_form.password1.data)
+        user_to_create = User(username=register_form.username.data, email=register_form.email.data, password_crypted =register_form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('views.market_page'))
@@ -20,3 +20,8 @@ def register_page():
             flash(f'There is an error when creating user : {err_msg}', category='danger')
     
     return render_template('register.html', form=register_form )
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login_page():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form )
