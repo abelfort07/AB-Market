62cf9f120995d1f1cb88039d12f999304d907538
Angelo Belfort <belfortangelo93@gmail.com>
2024-03-09 12:41:50 -0500

    add flask_bcrypt plugin for password encryption

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # create database 
 db = SQLAlchemy()
 DB_NAME = 'items.db'
+bcrypt = Bcrypt()
 
 def create_app():
     # Create the flask app
@@ -11,6 +13,7 @@ def create_app():
     app.config['SECRET_KEY'] = '55258efedbe4608330a7271c'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    bcrypt.init_app(app)
     app.app_context().push()
 
     # import and register blueprint
