9d7e95fe2c1c825db73e1d542a6ed90423ff79e5
Angelo Belfort <belfortangelo93@gmail.com>
2024-03-09 22:15:18 -0500

    import UserMixin and implement load_user function

--- website/models.py
+++ website/models.py
@@ -1,7 +1,15 @@
 from . import db
 from . import bcrypt
+from website import login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
@@ -16,6 +24,10 @@ class User(db.Model):
     @password_crypted.setter
     def password_crypted(self, plain_text_password):
         self.password = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+    
+    # check if db encrypted password matched with login form's password
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password, attempted_password)
         
     def __repr__(self) -> str:
         return f'User  {self.username}'
