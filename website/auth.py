64b75a7b533392a4d1eafcd6f44ed13503b16b8b
Angelo Belfort <belfortangelo93@gmail.com>
2024-03-09 22:14:44 -0500

    Implemented login functionality to login form.

--- website/auth.py
+++ website/auth.py
@@ -3,8 +3,10 @@ from flask import render_template, redirect, url_for
 from website.forms import RegisterForm, LoginForm
 from website.models import User
 from website import db
+from flask_login import login_user
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register_page():
@@ -21,7 +23,17 @@ def register_page():
     
     return render_template('register.html', form=register_form )
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login_page():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=login_form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=login_form.password.data):
+            login_user(attempted_user)
+            flash(f'Success You are logged in as : {attempted_user.username}', category='success')
+            return redirect(url_for('views.market_page'))
+        else:
+            flash('Username or Password are not matched! Please try again', category='danger')
+    
     return render_template('login.html', form=login_form )
